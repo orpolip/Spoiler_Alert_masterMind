bd5f955218bf48a559dbd4bd6d38d2636b96dd88
Péter Biró <biro.peter.hu@gmail.com>
2017-08-15 15:28:00 +0200

    user guesses displayed

---
+++ src/com/spoiler_alert/Display.java
@@ -0,0 +1,36 @@
+package com.spoiler_alert;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Display {
+
+    public static void main(String[] args) {
+
+        HashMap<Character, String> colorMap = new HashMap<>();
+        colorMap.put('r', "\u001B[0m"); //reset
+        colorMap.put('R', "\u001B[31m"); //red
+        colorMap.put('G', "\u001B[32m"); //green
+        colorMap.put('Y', "\u001B[33m"); //yellow
+        colorMap.put('B', "\u001B[34m"); //blue
+        colorMap.put('P', "\u001B[35m"); //purple
+        colorMap.put('C', "\u001B[36m"); //cyan
+
+        final String PEG = "\u25CF";
+
+        ArrayList<ArrayList<Character>> guesses = new ArrayList<>();
+        for (int i = 0; i < 4 ; i++) {
+            guesses.add(pickrandom.get_4_random());
+        }
+        int lineIndex = 1;
+        for ( ArrayList<Character> guess: guesses) {
+            System.out.print(lineIndex + ":   ");
+            lineIndex++;
+            for (Character color: guess) {
+                System.out.print(colorMap.get(color) + "  " + PEG);
+            }
+            System.out.println(colorMap.get('r') + "\n");
+        }
+    }
+}
+
