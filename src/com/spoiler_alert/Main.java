75365314db8431564825f92274cf5b4e02400724
akosENagy <akos.e.nagy@gmail.com>
2017-08-16 00:51:47 +0200

    Added menu and display to main, also cleaned it up a bit

--- src/com/spoiler_alert/Main.java
+++ src/com/spoiler_alert/Main.java
@@ -1,32 +1,41 @@
 package com.spoiler_alert;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class Main {
 
+    static final String VICTORY = "BBBB";
+    static final int MAX_TURNS = 10;
+
     public static void main(String[] args) {
 
-        ArrayList outcome = pickrandom.get_4_random();
+        ArrayList<Character> outcome = new ArrayList<>();
+        ArrayList<ArrayList<Character>> guesses = new ArrayList<>();
+
+        int menuChoice = Menu.menu();
+        if (menuChoice == 1) { // 1. : play with computer
+            outcome = pickrandom.get_4_random();
+        }
+        else if (menuChoice == 2) { // 2. : play with human
+            outcome = pickrandom.get_4_random();  // TODO: get user input for the outcome
+        }
         System.out.println("The outcome of the game is: " + outcome);
+
         String result;
-        String victory = "BBBB";
-        int Counter = 0;
-            do {
-               String guess = Userguess.get_user_input();
-                ArrayList guessArray = Userguess.input_to_array(guess);
-                result = Evaluator.evaluateGuess(guessArray, outcome);
-                System.out.println(result);
-                Counter += 1;
-                if( Counter == 3) {
-                    break;
-                }
-            }while (!result.equals(victory));
-        if( Counter == 3) {
+        int turnCount = 0;
+        do {
+            String guess = Userguess.get_user_input();
+            ArrayList guessArray = Userguess.input_to_array(guess);
+            guesses.add(guessArray);
+            result = Evaluator.evaluateGuess(guessArray, outcome);
+            Display.displayGuesses(guesses, outcome);
+        } while (!result.equals(VICTORY) && ++turnCount != MAX_TURNS);
+
+        if (turnCount == MAX_TURNS) {  // Lost the game
             System.out.println("U suck!");
         }
-        else{
+        else {  // Won the game
             System.out.println("Well done u lil shit, now go and fuck yourself");
         }
     }
