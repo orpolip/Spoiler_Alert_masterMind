08d0f0178d22d996c1a451dadaab7cb292eb6aca
Péter Biró <biro.peter.hu@gmail.com>
2017-08-15 15:42:17 +0200

    displayGuesses method added & evaluate is displayed

--- src/com/spoiler_alert/Display.java
+++ src/com/spoiler_alert/Display.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class Display {
 
-    public static void main(String[] args) {
+    public static void displayGuesses(ArrayList<ArrayList<Character>> guesses, ArrayList<Character> solution){
 
         HashMap<Character, String> colorMap = new HashMap<>();
         colorMap.put('r', "\u001B[0m"); //reset
@@ -18,10 +18,6 @@ public class Display {
 
         final String PEG = "\u25CF";
 
-        ArrayList<ArrayList<Character>> guesses = new ArrayList<>();
-        for (int i = 0; i < 4 ; i++) {
-            guesses.add(pickrandom.get_4_random());
-        }
         int lineIndex = 1;
         for ( ArrayList<Character> guess: guesses) {
             System.out.print(lineIndex + ":   ");
@@ -29,8 +25,25 @@ public class Display {
             for (Character color: guess) {
                 System.out.print(colorMap.get(color) + "  " + PEG);
             }
-            System.out.println(colorMap.get('r') + "\n");
+            System.out.println(colorMap.get('r') + "    "+ Evaluator.evaluateGuess(guess, solution) +"\n");
         }
+    }
+
+
+    /***********************
+     * only for test cases *
+     **********************/
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<Character>> guesses = new ArrayList<>();
+        for (int i = 0; i < 4 ; i++) {
+            guesses.add(pickrandom.get_4_random());
+        }
+
+        ArrayList<Character> solution = pickrandom.get_4_random();
+
+        displayGuesses(guesses, solution);
+
     }
 }
 
