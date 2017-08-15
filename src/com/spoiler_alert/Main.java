4ae1f5d3fea5bd23c0bbae6fbfc79b24ecad8898
orpolip <galdonyi0109@gmail.com>
2017-08-15 11:05:28 +0200

    pick 4 random strings feature added

---
+++ src/com/spoiler_alert/Main.java
@@ -0,0 +1,14 @@
+package com.spoiler_alert;
+
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List outcome = pickrandom.get_4_random();
+        System.out.println("The outcome of the game is: " + outcome);
+
+    }
+}
