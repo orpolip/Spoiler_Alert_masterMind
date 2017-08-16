1354f654a23d4b374d340da7c9c555bf7fd8784f
akosENagy <akos.e.nagy@gmail.com>
2017-08-16 13:47:53 +0200

    evaluation is fixed. uses clone for the guesses too now.

--- src/com/spoiler_alert/Evaluator.java
+++ src/com/spoiler_alert/Evaluator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Evaluator {
 
-    public static String evaluateGuess(ArrayList<Character> guess, ArrayList<Character> solutionReference) {
+    public static String evaluateGuess(ArrayList<Character> guessReference, ArrayList<Character> solutionReference) {
         /*
         Compares a list of guesses to the solution and returns an evaluation.
         Black 'peg' for full hits (right color right place),
@@ -13,23 +13,29 @@ public class Evaluator {
         guess & solutionReference: ArrayLists of color name initials as characters
         Returns a string with 4 characters as an evaluation.
         */
-        ArrayList<Character> solutionClone = new ArrayList<>(solutionReference); // Clone it so the original stays intact
+        ArrayList<Character> guessClone = new ArrayList<>(guessReference);
+        ArrayList<Character> solutionClone = new ArrayList<>(solutionReference); // Clone them so the originals stay intact
         String eval = "";
 
-        for (int i = 0; i < guess.size(); i++) {  // Find all the full hits first
-            if (guess.get(i) == solutionClone.get(i)) {
+        for (int i = 0; i < guessReference.size(); i++) {  // Find all the full hits first
+            if (guessClone.get(i) == solutionClone.get(i)) {
                 eval += "B";
-                solutionClone.set(i, null); // to get it out of the way on later iterations
+                guessClone.set(i, null);
+                solutionClone.set(i, null); // to get them out of the way on later iterations
             }
         }
 
-        for (int i = 0; i < guess.size(); i++) { // Then the partial hits
-            char currentGuess = guess.get(i);
-            int indexInSolution = solutionClone.indexOf(currentGuess);
+        for (int i = 0; i < guessReference.size(); i++) { // Then the partial hits
 
-            if (indexInSolution != -1) {  // If it's contained in the solution elsewhere, it's a partial hit
-                eval += "W";
-                solutionClone.set(indexInSolution, null);
+            Character currentGuess = guessClone.get(i);
+
+            if (currentGuess != null) {  // Only check the ones that haven't been evaluated yet
+                int indexInSolution = solutionClone.indexOf(currentGuess);
+
+                if (indexInSolution != -1) {  // If it's contained in the solution elsewhere, it's a partial hit
+                    eval += "W";
+                    solutionClone.set(indexInSolution, null);
+                }
             }
         }
 
