031e639b3ce02c1bd37c4bff149a31226c5b9776
Péter Biró <biro.peter.hu@gmail.com>
2017-08-17 15:45:25 +0200

    Menu updated with autosolve

--- src/com/spoiler_alert/Menu.java
+++ src/com/spoiler_alert/Menu.java
@@ -8,28 +8,27 @@ public class Menu {
 
 
         System.out.println("Make your choice: ");
-        int choice = 0;
-        while (choice == 0) {
+        int choice = -1;
+        while (choice == -1) {
             Scanner scan = new Scanner(System.in);
             if(scan.hasNextInt()){choice = scan.nextInt();}
-            if (choice != 1 && choice != 2 && choice != 3) {
-                choice = 0;
-                System.out.println("Type  the corresponding number from the above menu!");
-            } else if (choice == 4){
-                System.out.println("Exiting..");
-                System.exit(0);
-                            }
-            else if(choice == 3){
-                System.out.println("Pick four of the available colors : red, green, yellow, blue, purple, cyan");
-                System.out.println("You must type only the first letter of a color, don't worry they are not case sensitive ");
-                System.out.println("But don't even think to make some tricky typo like:");
-                System.out.println("GGG or bbbbb or xXxX or greenish red pink black, etc.\n");
-                break;
+            switch (choice){
+                case 0:
+                    System.out.println("Exiting..");
+                    System.exit(0);
+                    break;
+                case 1 : break;
+                case 2 : break;
+                case 3 : break;
+                case 4 : System.out.println("Pick four of the available colors : red, green, yellow, blue, purple, cyan");
+                        System.out.println("You must type only the first letter of a color, don't worry they are not case sensitive ");
+                        System.out.println("But don't even think to make some tricky typo like:");
+                        System.out.println("GGG or bbbbb or xXxX or greenish red pink black, etc.\n");
+                        break;
+                default: choice = -1;
+                        System.out.println("Type  the corresponding number from the above menu!");
             }
-            else if (choice == 1 || choice == 2) {
-                break;
-            }
-        }
+         }
         return choice;
     }
 
@@ -40,8 +39,9 @@ public class Menu {
         System.out.format("|        MENU            |%n");
         System.out.format("|1. Play against computer|%n");
         System.out.format("|2. Play against player  |%n");
-        System.out.format("|3. Show how to play     |%n");
-        System.out.format("|4. Exit                 |%n");
+        System.out.format("|3. Computer plays       |%n");
+        System.out.format("|4. Show how to play     |%n");
+        System.out.format("|0. Exit                 |%n");
         System.out.format("+------------------------+%n");
 
     }
